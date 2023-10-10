c2eaa37b1d9726fca5d6dc2aadd5534155fb6fcd
loveheenavasu <trivediheenalove@gmail.com>
2023-10-10 18:30:40 +0530

    changes in message page

--- src/components/Messages.jsx
+++ src/components/Messages.jsx
@@ -27,40 +27,31 @@ import {
   TextField,
   Typography,
 } from "@mui/material";
-import { makeStyles } from "@mui/styles";
 import SendRoundedIcon from "@mui/icons-material/SendRounded";
 
 const db = getFirestore(app);
 
-const useStyles = makeStyles({
-  table: {
-    minWidth: 650,
-  },
-  chatSection: {
-    width: "100%",
-    height: "66vh",
-  },
-  headBG: {
-    backgroundColor: "#e0e0e0",
-  },
-  borderRight500: {
-    borderRight: "1px solid #e0e0e0",
-  },
-  messageArea: {
-    height: "55vh",
-    overflowY: "auto",
-  },
-  Users: {
-    height: "40vh",
-    overflowY: "auto",
-  },
-});
-
 const Messages = ({ userData }) => {
-  const classes = useStyles();
   const [users, setUsers] = useState([]);
   const [messages, setMessages] = useState([]);
   const [currentMessage, setCurrentMessage] = useState("");
+  
+  // CSS styles applied directly to components
+  const chatSectionStyle = {
+    width: "100%",
+    height: "66vh",
+  };
+
+  const usersListStyle = {
+    height: "40vh",
+    overflowY: "auto",
+  };
+
+  const messageAreaStyle = {
+    height: "55vh",
+    overflowY: "auto",
+  };
+
   useEffect(() => {
     // Fetch users from Firestore
     const fetchUsers = async () => {
@@ -73,7 +64,7 @@ const Messages = ({ userData }) => {
     fetchUsers();
   }, []);
 
-  //   // Function to send a message
+  // Function to send a message
   const sendMessage = async () => {
     if (currentMessage.trim() === "") return;
 
@@ -103,13 +94,13 @@ const Messages = ({ userData }) => {
       unsubscribe();
     };
   }, []);
+  
   const isCurrentUser = (message) => {
-    // Replace userId with the actual user ID of the current user
     return message.senderId === userData?.id;
   };
-  console.log(messages, "msgs");
+
   function formatFirestoreTimestamp(timestamp) {
-    const date = new Date(timestamp?.seconds * 1000); // Convert Firestore timestamp to JavaScript Date
+    const date = new Date(timestamp?.seconds * 1000);
     const hours = date.getHours();
     const minutes = date.getMinutes();
     const ampm = hours >= 12 ? "PM" : "AM";
@@ -128,8 +119,8 @@ const Messages = ({ userData }) => {
           </Typography> */}
         </Grid>
       </Grid>
-      <Grid container component={Paper} className={classes.chatSection}>
-        <Grid item xs={3} className={classes.borderRight500}>
+      <Grid container component={Paper} style={chatSectionStyle}>
+        <Grid item xs={3} style={{ borderRight: "1px solid #e0e0e0" }}>
           <List>
             <ListItem button key="RemySharp">
               <ListItemIcon>
@@ -144,7 +135,6 @@ const Messages = ({ userData }) => {
               Users List
             </Typography>
           </List>
-          {/* sender */}
           <Divider />
           {/* <Grid item xs={12} style={{ padding: "10px" }}>
             <TextField
@@ -155,8 +145,7 @@ const Messages = ({ userData }) => {
             />
           </Grid> */}
           <Divider />
-
-          <List className={classes.Users}>
+          <List style={usersListStyle}>
             {users.map((user) => (
               <ListItem key="RemySharp">
                 <ListItemIcon>
@@ -172,36 +161,14 @@ const Messages = ({ userData }) => {
           </List>
         </Grid>
         <Grid item xs={9}>
-          <List className={classes.messageArea}>
+          <List style={messageAreaStyle}>
             {messages?.map((message, index) => (
-              // <div
-              //   key={index}
-              //   style={{
-              //     backgroundColor: ? "grey" : "green",
-              //   }}
-              //   className={isCurrentUser(message) ? "sent" : "received"}
-              // >
-              //   <p>{message.text}</p>
-              // </div>
-
               <ListItem key={index}>
                 <Grid container>
                   <Grid item xs={12}>
                     <ListItemText
                       align={isCurrentUser(message) ? "right" : "left"}
                       primary={message.text}
-                      //   sx={{
-                      //     backgroundColor: isCurrentUser(message)
-                      //       ? "grey"
-                      //       : "green",
-                      //   }}
-                      //   sx={{
-                      //     width: isCurrentUser(message) ? "50%": "50%",
-                      //     background: "#8080800d",
-                      //     color: " black",
-                      //     padding: "10px",
-                      //     borderRadius: "11px 15px 0px 15px",
-                      //   }}
                     >
                       {message.text}
                     </ListItemText>
@@ -209,15 +176,9 @@ const Messages = ({ userData }) => {
                   <Grid
                     item
                     xs={12}
-                    sx={{
-                      textAlign: isCurrentUser(message) ? "right" : "left",
-                    }}
+                    style={{ textAlign: isCurrentUser(message) ? "right" : "left" }}
                   >
-                    <span
-                      //   align={isCurrentUser(message) ? "right" : "left"}
-                      style={{ fontSize: "10px" , marginRight:"10px"}}
-                      //   secondary={formatFirestoreTimestamp(message.timestamp)}
-                    >
+                    <span style={{ fontSize: "10px", marginRight: "10px" }}>
                       {formatFirestoreTimestamp(message.timestamp)}
                     </span>
                     <span style={{ fontSize: "12px" }}>
@@ -242,7 +203,7 @@ const Messages = ({ userData }) => {
               <Button
                 onClick={sendMessage}
                 variant="contained"
-                sx={{
+                style={{
                   borderRadius: "50%",
                   display: "flex",
                   justifyContent: "center",
